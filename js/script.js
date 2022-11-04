7070100756ea292423684af9dbe629c4cf9e426a
Emanuele213 <vittorius213@gmail.com>
2022-11-04 17:11:06 +0100

    add bottom and random number

---
+++ js/script.js
@@ -0,0 +1,46 @@
+let numRandom;
+const eleNumber = document.querySelector('.num');
+const eleBtn = document.querySelector('.mod-btn');
+let eleBtnConfront = document.querySelector('.mod-btn-blue');
+
+eleBtn.addEventListener('click', function () {
+    eleBtn.classList.add('hidden');
+    function number () {
+        for (let i = 0; i < 5; i++) {
+            numRandom = Math.floor(Math.random() * 100 ) + 1;
+            console.log(numRandom);
+            eleNumber.innerHTML += `${numRandom} ` ;
+        }
+    }
+    
+    number ();
+    
+    const idWait = setTimeout (waitNumber, 10000);
+    function waitNumber () {
+        eleNumber.classList.add('hidden');
+    }
+
+    const idWaitBar = setTimeout (waitBar, 5000);
+    function waitBar () {
+        const container = document.querySelector('.container');
+        const Bar = document.createElement('input');
+        Bar.setAttribute("type", "number");
+        Bar.setAttribute("name", "number");
+        Bar.setAttribute("id", "number");
+        container.append(Bar);
+        const eleInput = document.querySelector('#number');
+        const eleBar = eleInput.value;
+        eleBtnConfront = document.createElement('button');
+        eleBtnConfront.classList.add('mod-btn-blue');
+        eleBtnConfront.innerHTML = "Controlla";
+        container.append(eleBtnConfront);
+
+        eleBtnConfront.addEventListener('click', function() {
+            if (numRandom == eleBar.value) {
+                console.log('Hai vinto');
+            }else {
+                console.log('Hai perso');
+            }
+        })
+    }
+}) 
